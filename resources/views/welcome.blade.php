e59155d81c96fce31b518f2903e22d75a2970950
b9Mike <migu31.z.a@gmail.com>
2025-06-02 23:38:42 -0600

    feat: se cambio la card por una tabla y se implemento las vistas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,9 @@
     <title>URL Shortener</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous">
+    </script>
 </head>
 
 <body class="bg-light">
@@ -38,9 +40,41 @@
             <div class="col-md-7 mb-4">
                 <div class="card p-4 shadow">
                     <h5 class="card-title text-center">URLs Existentes</h5>
-                    <ul id="urlList" class="list-group"></ul>
+                    {{-- <ul id="urlList" class="list-group"></ul> --}}
+                    <div id="kt_table_users_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
+                        <div class="table-responsive">
+                            <table class="table align-middle table-row-dashed fs-6 gy-5 dataTable no-footer"
+                                id="kt_table_users">
+                                <thead>
+                                    <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
+                                        <th class="min-w-125px" tabindex="0" aria-controls="kt_table_users"
+                                            aria-label="Urls">
+                                            Urls
+                                        </th>
+                                        <th class="min-w-125px" tabindex="0" aria-controls="kt_table_users"
+                                            aria-label="Estatsus">
+                                            Estatus
+                                        </th>
+                                        <th class="min-w-125px" tabindex="0" aria-label="Vistas">
+                                            Vistas
+                                        </th>
+                                        <th class="text-end pe-3 min-w-100px" aria-label="Acciones">
+                                            Acciones
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody id="urlTableBody" class="text-gray-600 fw-semibold">
+                                    <tr>
+                                        <td valign="top" colspan="4" class="text-center dataTables_empty">Cargando...</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            {{-- estaduisticas --}}
             <div class="col-md-5 mb-4">
                 <div class="card p-4 shadow">
                     <h5 class="card-title text-center">Estadísticas</h5>
@@ -100,87 +134,97 @@
             const res = await fetch("{{ route('url.urls') }}");
             const urls = await res.json();
 
-            const lista = document.getElementById("urlList");
-            lista.innerHTML = ""; // Limpiar antes de cargar
+            const tableBody = document.getElementById("urlTableBody");
+            tableBody.innerHTML = ""; // Limpiar antes de cargar
+
+            if (urls.length === 0) {
+                tableBody.innerHTML = `
+                    <tr>
+                        <td colspan="4" class="text-center">No se han encontrado registros</td>
+                    </tr>
+                `;
+                return;
+            }
 
             urls.forEach((url, index) => {
-                const li = document.createElement("li");
-                li.className = "list-group-item d-flex justify-content-between align-items-center";
+                const estadoBadge = url.is_active ?
+                    `<span class="badge bg-success">Activo</span>` :
+                    `<span class="badge bg-danger">Desactivado</span>`;
 
-                const estadoBadge = url.is_active
-                ? `<span class="badge bg-success ms-2">Activo</span>`
-                : `<span class="badge bg-danger ms-2">Desactivado</span>`;
+                const row = document.createElement("tr");
 
-                li.innerHTML = `
-                    <div>
-                        <a href="${url.short_url}" target="_blank">${url.short_url}</a> - </strong>
-                        ${estadoBadge}
-                    </div>
-                    <div class="dropdown">
-                        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu${index}" data-bs-toggle="dropdown" aria-expanded="false">
-                            Acciones
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu${index}">
-                            <li><a class="dropdown-item reactivar-btn" href="#" data-code="${url.short_code}">Reactivar</a></li>
-                            <li><a class="dropdown-item deactivate-btn" href="#" data-code="${url.short_code}">Desactivar</a></li>
-                        </ul>
-                    </div>
+                row.innerHTML = `
+                    <td>
+                        <a href="${url.short_url}" target="_blank">${url.short_url}</a>
+                    </td>
+                    <td>${estadoBadge}</td>
+                    <td>${url.visits}</td>
+                    <td class="text-end">
+                        <div class="dropdown">
+                            <button class="btn btn-sm btn-primary dropdown-toggle" type="button" id="dropdownMenu${index}" data-bs-toggle="dropdown" aria-expanded="false">
+                                Acciones
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenu${index}">
+                                <li><a class="dropdown-item reactivar-btn" href="#" data-code="${url.short_code}">Reactivar</a></li>
+                                <li><a class="dropdown-item deactivate-btn" href="#" data-code="${url.short_code}">Desactivar</a></li>
+                            </ul>
+                        </div>
+                    </td>
                 `;
 
-                lista.appendChild(li);
+                tableBody.appendChild(row);
             });
-
         }
 
+
         //reactivar url
-        document.addEventListener('click', function(e) {
+        document.getElementById('urlTableBody').addEventListener('click', function(e) {
             if (e.target.classList.contains('reactivar-btn')) {
                 e.preventDefault();
                 const code = e.target.getAttribute('data-code');
 
                 fetch(`/reactivate-url/${code}`, {
-                    method: 'PATCH',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json',
-                        'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                    }
-                })
-                .then(res => {
-                    if (!res.ok) throw new Error("Error al reactivar la URL");
-                    return res.json();
-                })
-                .then(data => {
-                    alert("URL reactivada con éxito");
-                    cargarUrls();
-                })
-                .catch(err => {
-                    alert("Hubo un error: " + err.message);
-                });
-            }
-            else if (e.target.classList.contains("deactivate-btn")) {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                        }
+                    })
+                    .then(res => {
+                        if (!res.ok) throw new Error("Error al reactivar la URL");
+                        return res.json();
+                    })
+                    .then(data => {
+                        alert("URL reactivada con éxito");
+                        cargarUrls();
+                    })
+                    .catch(err => {
+                        alert("Hubo un error: " + err.message);
+                    });
+            } else if (e.target.classList.contains("deactivate-btn")) {
                 e.preventDefault();
                 const code = e.target.getAttribute("data-code");
-                
+
                 fetch(`/deactivate-url/${code}`, {
-                    method: 'PATCH',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json',
-                        'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                    }
-                })
-                .then(res => {
-                    if (!res.ok) throw new Error("Error al desactivar la URL");
-                    return res.json();
-                })
-                .then(data => {
-                    alert("URL desactivada con éxito");
-                    cargarUrls();
-                })
-                .catch(err => {
-                    alert("Hubo un error: " + err.message);
-                });
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                        }
+                    })
+                    .then(res => {
+                        if (!res.ok) throw new Error("Error al desactivar la URL");
+                        return res.json();
+                    })
+                    .then(data => {
+                        alert("URL desactivada con éxito");
+                        cargarUrls();
+                    })
+                    .catch(err => {
+                        alert("Hubo un error: " + err.message);
+                    });
             }
         });
 
